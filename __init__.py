ef26535b9b6d06fc66c080d9427898e208aef240
Tim Masliuchenko <timfjord@hey.com>
2023-04-07 16:50:57 +0100

    Add public api

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+Public API for the Projectionist package.
+Can be used for finding alternate files.
+See README.md for more information.
+"""
+
+__all__ = ["find_alternate_file"]
+
+
+def find_alternate_file(root, relpath):
+    pass
